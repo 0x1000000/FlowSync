77df03b59e29fd5542bd49e03dea4ff2a2532fca
0x1000000 <25109784+0x1000000@users.noreply.github.com>
2025-05-13 23:40:24 -0400

    StandAloneApp

---
+++ WebDemoStandalone/Layout/NavMenu.razor.cs
@@ -0,0 +1,21 @@
+﻿using WebDemoStandalone.Metadata;
+
+namespace WebDemoStandalone.Layout;
+
+public partial class NavMenu : ISyncDemoPageModeVisitor<string>
+{
+    public static readonly IReadOnlyList<SyncDemoPageMode> Modes = new []
+        { SyncDemoPageMode.NoSync, SyncDemoPageMode.UseFirst, SyncDemoPageMode.UseLast, SyncDemoPageMode.Queue, SyncDemoPageMode.DeBounce };
+
+    public string GetPageModeUrl(SyncDemoPageMode syncDemoPageMode) => syncDemoPageMode.Accept(this);
+
+    string ISyncDemoPageModeVisitor<string>.CaseNoSync() => $"demo/{nameof(SyncDemoPageMode.NoSync)}";
+
+    string ISyncDemoPageModeVisitor<string>.CaseUseFirst() => $"demo/{nameof(SyncDemoPageMode.UseFirst)}";
+
+    string ISyncDemoPageModeVisitor<string>.CaseUseLast() => $"demo/{nameof(SyncDemoPageMode.UseLast)}";
+
+    string ISyncDemoPageModeVisitor<string>.CaseQueue() => $"demo/{nameof(SyncDemoPageMode.Queue)}";
+
+    string ISyncDemoPageModeVisitor<string>.CaseDeBounce() => $"demo/{nameof(SyncDemoPageMode.DeBounce)}";
+}
